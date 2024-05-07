0aa941c8eb02c0c8de1d3c6ddbb1d9daf828e49c
X250 <aleksander.synoradzki@gmail.com>
2024-05-07 14:48:02 +0200

    RabbitMQ Consumer implemented

---
+++ rabbit-consumer/src/main/java/com/x250/rabbitconsumer/consumer/RabbitMQGetConsumer2.java
@@ -0,0 +1,27 @@
+package com.x250.rabbitconsumer.consumer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class RabbitMQGetConsumer2 {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/get_rabbit2")
+    ResponseEntity<String> getRabbit() {
+        Object message = rabbitTemplate.receiveAndConvert(queue);
+        return ResponseEntity.ok(message.toString());
+    }
+
+}
