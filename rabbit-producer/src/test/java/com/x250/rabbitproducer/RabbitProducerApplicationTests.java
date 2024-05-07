f04ec851611becad1de605a99f472f94be4068df
X250 <aleksander.synoradzki@gmail.com>
2024-05-07 11:12:56 +0200

    rabbit-mq-producer initialized

---
+++ rabbit-producer/src/test/java/com/x250/rabbitproducer/RabbitProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x250.rabbitproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
